6d2d3977584e86987036400c8b793d9044080ad7
Nurzhan Nogerbek <nurzhannogerbek@gmail.com>
2021-01-19 18:30:57 +0600

    BT-94

--- src/aws_lambda_functions/send_message_from_facebook_messenger/lambda_function.py
+++ src/aws_lambda_functions/send_message_from_facebook_messenger/lambda_function.py
@@ -1,6 +1,6 @@
 import logging
 import os
-import json
+from typing import *
 
 # Configure the logging tool in the AWS Lambda function.
 logger = logging.getLogger(__name__)
@@ -15,10 +15,38 @@ POSTGRESQL_DB_NAME = os.environ["POSTGRESQL_DB_NAME"]
 TELEGRAM_API_URL = "https://api.telegram.org"
 APPSYNC_CORE_API_URL = os.environ["APPSYNC_CORE_API_URL"]
 APPSYNC_CORE_API_KEY = os.environ["APPSYNC_CORE_API_KEY"]
+FACEBOOK_MESSENGER_BOT_VERIFY_TOKEN = os.environ["FACEBOOK_MESSENGER_BOT_VERIFY_TOKEN"]
 
 # The connection to the database will be created the first time the AWS Lambda function is called.
 # Any subsequent call to the function will use the same database connection until the container stops.
 POSTGRESQL_CONNECTION = None
+
+
+def set_webhook_for_facebook_messenger(query_string_parameters: Dict[AnyStr, Any]) -> Dict[AnyStr, Any]:
+    # Define all necessary variables.
+    hub_mode = query_string_parameters.get("hub.mode", None)
+    hub_challenge = query_string_parameters.get("hub.challenge", None)
+    hub_verify_token = query_string_parameters.get("hub.verify_token", None)
+
+    # Check the values of query parameters.
+    if hub_mode == "subscribe" and hub_challenge:
+        # Check the verify token value.
+        if hub_verify_token != FACEBOOK_MESSENGER_BOT_VERIFY_TOKEN:
+            return {
+                "statusCode": 403,
+                "body": "Verification token mismatch. Check your 'VERIFY_TOKEN'."
+            }
+
+        # You must echo back the "hub.challenge" value, when the endpoint is registered as a webhook.
+        return {
+            "statusCode": 200,
+            "body": hub_challenge
+        }
+
+    # Return the status code 200.
+    return {
+        "statusCode": 200
+    }
 
 
 def lambda_handler(event, context):
@@ -26,22 +54,23 @@ def lambda_handler(event, context):
     :param event: The AWS Lambda function uses this parameter to pass in event data to the handler.
     :param context: The AWS Lambda function uses this parameter to provide runtime information to your handler.
     """
-    # Parse all necessary query and path parameters. Also parse the JSON object body.
+    # Define the http method of the request.
+    try:
+        http_method = event["requestContext"]["http"]["method"]
+    except Exception as error:
+        logger.error(error)
+        raise Exception(error)
+    # Parse all necessary query parameters.
     try:
         query_string_parameters = event["queryStringParameters"]
     except Exception as error:
         logger.error(error)
         raise Exception(error)
-    try:
-        path_parameters = event["pathParameters"]
-    except Exception as error:
-        logger.error(error)
-        raise Exception(error)
-    try:
-        body = json.loads(event["body"])
-    except Exception as error:
-        logger.error(error)
-        raise Exception(error)
+
+    # Check the http method of the request.
+    if http_method == "GET":
+        # Set the webhook for facebook messenger bot.
+        set_webhook_for_facebook_messenger(query_string_parameters)
 
     # Return nothing.
     return None
